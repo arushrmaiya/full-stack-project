30584c8dde957896848330666e84f1bf58f0af12
arushrmaiya <arushmaiyar@gmail.com>
2025-10-27 10:26:43 -0400

    added js tutorial, changed org of files

---
+++ Tutorial/Foundations/javascript/Foundations/javascript-exercises-main/03_numberChecker/numberChecker.js
@@ -0,0 +1,12 @@
+function numberChecker(number) {
+  // if (number === 6) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+
+  return (number >=10);
+}
+
+// Do not edit below this line
+module.exports = numberChecker;
---
+++ Tutorial/Foundations/javascript/tutorial 1/javascript.js
@@ -0,0 +1,6 @@
+console.log(23+97);
+
+console.log((4+6+9)/77);
+
+let a = 10;
+
